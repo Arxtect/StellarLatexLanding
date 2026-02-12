94319cfdeee62feadcb7ddc1a1f2f6abca8aec44
tommyjqliu <tommyjqliu@outlook.com>
2026-02-12 15:34:06 +0800

    fix: pdf select

--- components/demo-editor/pdf-viewer.tsx
+++ components/demo-editor/pdf-viewer.tsx
@@ -1,5 +1,6 @@
 import "react-pdf/dist/Page/AnnotationLayer.css";
 import "react-pdf/dist/Page/TextLayer.css";
+import "./pdf-viewer-style.css";
 import dynamic from "next/dynamic";
 
 export * from "./pdf-viewer-store";
