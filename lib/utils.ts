6a4679de118cf714380d07679b0f1ad92349732e
tommyjqliu <tommyjqliu@outlook.com>
2026-02-14 14:13:02 +0800

    feat: update repo url

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
-export const GITHUB_REPO_URL = "https://github.com/Arxtect/StellarLatex";
+export const GITHUB_REPO_URL = "https://github.com/Arxtect/StellarLatex2";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
